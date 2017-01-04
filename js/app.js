ce94860d1e558b0cd82afd7f79edde069bd941e8
tunadao1 <hugorafaelmc@gmail.com>
2017-01-04 16:26:52 -0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,32 @@
+var app = angular.module('app', []);
+
+app.controller('appController', function($scope) {
+
+	$scope.toggleMenu = function(){
+
+		if($scope.menu){
+			$scope.menu = '';
+		}else{
+
+			$scope.menu = {'left': 0};
+			
+		}
+
+	}
+
+	$scope.filmes = 
+	[
+	{
+		"titulo"   : "Star Wars: Episode III",
+		"subtitulo": "2005",
+		"nota"	   : 8.5,
+		"capa"     : "Star-Wars-Episode-III-Revenge-Of-The-Sith"
+	},{
+		"titulo"   : "Star Wars: Episode II",
+		"subtitulo": "2002",
+		"nota"	   : 8,
+		"capa"     : "Star-Wars-Episode-II-Attack-Of-The-Clones"
+	}
+	]
+
+});
