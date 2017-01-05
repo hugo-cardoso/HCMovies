a374bd0ad041a0910321f201063846d28606774b
tunadao1 <hugorafaelmc@gmail.com>
2017-01-05 17:42:18 -0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -27,60 +27,40 @@ app.controller('appController', function($scope, $rootScope) {
 	$rootScope.filmes = 
 	[
 	{
+		"titulo"   : "Star Wars: Episode I",
+		"subtitulo": "1999",
+		"nota"	   : 6.5,
+		"capa"     : "Star-Wars-Episode-I-The-Phantom-Menace"
+	},{
+		"titulo"   : "Star Wars: Episode II",
+		"subtitulo": "2002",
+		"nota"	   : 6.7,
+		"capa"     : "Star-Wars-Episode-II-Attack-Of-The-Clones"
+	},{
 		"titulo"   : "Star Wars: Episode III",
 		"subtitulo": "2005",
-		"nota"	   : 8.5,
+		"nota"	   : 7.6,
 		"capa"     : "Star-Wars-Episode-III-Revenge-Of-The-Sith"
 	},{
-		"titulo"   : "Star Wars: Episode II",
-		"subtitulo": "2002",
-		"nota"	   : 8,
-		"capa"     : "Star-Wars-Episode-II-Attack-Of-The-Clones"
+		"titulo"   : "Star Wars: Episode IV",
+		"subtitulo": "1997",
+		"nota"	   : 8.7,
+		"capa"     : "Star-Wars-Episode-IV-A-New-Hope"
 	},{
-		"titulo"   : "Star Wars: Episode II",
-		"subtitulo": "2002",
-		"nota"	   : 8,
-		"capa"     : "Star-Wars-Episode-II-Attack-Of-The-Clones"
+		"titulo"   : "Star Wars: Episode V",
+		"subtitulo": "1980",
+		"nota"	   : 8.8,
+		"capa"     : "Star-Wars-Episode-V-The-Empire-Strikes-Back"
 	},{
-		"titulo"   : "Star Wars: Episode II",
-		"subtitulo": "2002",
-		"nota"	   : 8,
-		"capa"     : "Star-Wars-Episode-II-Attack-Of-The-Clones"
+		"titulo"   : "Star Wars: Episode VI",
+		"subtitulo": "1983",
+		"nota"	   : 8.4,
+		"capa"     : "Star-Wars-Episode-VI-Return-Of-The-Jedi"
 	},{
-		"titulo"   : "Star Wars: Episode II",
-		"subtitulo": "2002",
-		"nota"	   : 8,
-		"capa"     : "Star-Wars-Episode-II-Attack-Of-The-Clones"
-	},{
-		"titulo"   : "Star Wars: Episode II",
-		"subtitulo": "2002",
-		"nota"	   : 8,
-		"capa"     : "Star-Wars-Episode-II-Attack-Of-The-Clones"
-	},{
-		"titulo"   : "Star Wars: Episode II",
-		"subtitulo": "2002",
-		"nota"	   : 8,
-		"capa"     : "Star-Wars-Episode-II-Attack-Of-The-Clones"
-	},{
-		"titulo"   : "Star Wars: Episode II",
-		"subtitulo": "2002",
-		"nota"	   : 8,
-		"capa"     : "Star-Wars-Episode-II-Attack-Of-The-Clones"
-	},{
-		"titulo"   : "Star Wars: Episode II",
-		"subtitulo": "2002",
-		"nota"	   : 8,
-		"capa"     : "Star-Wars-Episode-II-Attack-Of-The-Clones"
-	},{
-		"titulo"   : "Star Wars: Episode II",
-		"subtitulo": "2002",
-		"nota"	   : 8,
-		"capa"     : "Star-Wars-Episode-II-Attack-Of-The-Clones"
-	},{
-		"titulo"   : "Star Wars: Episode II",
-		"subtitulo": "2002",
-		"nota"	   : 8,
-		"capa"     : "Star-Wars-Episode-II-Attack-Of-The-Clones"
+		"titulo"   : "Star Wars: Episode VII",
+		"subtitulo": "2005",
+		"nota"	   : 8.1,
+		"capa"     : "Star-Wars-Episode-VII-The-Force-Awakens"
 	}
 	];
 
